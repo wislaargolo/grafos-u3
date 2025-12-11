49c0d41d610839a61840ebfa52c34a6b9c52ac91
Daniel Lucena <rlucena.daniel@gmail.com>
2025-12-11 11:08:17 -0300

    Finaliza memetico

--- algorithm/MemeticSearch.h
+++ algorithm/MemeticSearch.h
@@ -6,49 +6,73 @@
 #include "GeneticSearch.h"
 #include "LocalSearch.h"
 
-
 template <typename Node>
 void print_population(const IGraph<Node> &graph, const std::vector<std::vector<double>> &weights,
-                      std::vector<Individual>& population)
+                      std::vector<Individual> &population)
 {
     std::cout << "[Population]\n";
     for (size_t i = 0; i < population.size(); i++)
     {
-        double cost = calculate_path_cost(weights, population[i]);
+        double cost = population[i].cost;
         std::cout << "Individual " << i + 1 << ": Cost = " << cost << ", Path = ";
-        for (const auto &node : population[i])
+        for (const auto &node : population[i].path)
         {
             std::cout << graph.get_node(node) << " ";
         }
         std::cout << "\n";
     }
+
+    std::cout << "\n";
 };
 
 // (1) Inicio
 // Geração da População Inicial
-// GeneticSearch::generate_population()
 template <typename Node>
-std::vector<std::vector<int>> generate_initial_population(const IGraph<Node> &graph,
-                                                          const std::vector<std::vector<double>> &weights)
+std::vector<Individual> generate_initial_population(const IGraph<Node> &graph,
+                                                    const std::vector<std::vector<double>> &weights)
 {
-
     return generate_population(graph, weights);
 };
 
 // (2) Fitness
-void calculate_initial_fitness(std::vector<Individual>& population,
-    const std::vector<std::vector<double>>& weights){
-        calculate_fitness(population, weights);
-    }
+void calculate_initial_fitness(std::vector<Individual> &population,
+                               const std::vector<std::vector<double>> &weights)
+{
+    calculate_fitness(population, weights);
+};
 
 // (3) Nova Geração
+std::vector<Individual> generate_new_individuas(std::vector<Individual> &population, const std::vector<std::vector<double>> &weights, int iteration_count, std::pair<int, int> &last_parents)
+{
+    std::pair<int, int> parents = select_parents(population, iteration_count, last_parents);
+
+    // Cruzamento por crossover
+    std::vector<int> path1 = ordered_crossover(population[parents.first], population[parents.second]);
+    Individual child1 = {path1, -1, -1};
+
+    std::vector<int> path2 = ordered_crossover(population[parents.second], population[parents.first]);
+    Individual child2 = {path2, -1, -1};
+
+    // Mutação com taxa de 50%
+    apply_mutation(child1, MUTATION_PERCENT);
+    apply_mutation(child2, MUTATION_PERCENT);
+
+    child1.cost = calculate_path_cost(weights, child1.path);
+    child2.cost = calculate_path_cost(weights, child2.path);
+    child1.fitness = 1 / child1.cost;
+    child2.fitness = 1 / child2.cost;
+
+    std::vector<Individual> offsprings = {child1, child2};
+
+    return offsprings;
+};
 
 // (4) Busca local
 // Função para melhorar cada indivíduo da população usando busca local
 // Note: não é template porque o tipo de nó não é necessário aqui
-void improve_population(
+void improve_individuas(
     const std::vector<std::vector<double>> &weights,
-    std::vector<Individual>& population,
+    std::vector<Individual> &population,
     LocalSearchMethod method,
     ImprovementType improvement)
 {
@@ -64,25 +88,74 @@ void improve_population(
 };
 
 // (5) Renovar
+void renew_population(std::vector<Individual> &population, const std::vector<Individual> offsprings, const std::vector<std::vector<double>> &weights)
+{
+    population = renovation_elitism(population, offsprings, weights);
+};
 
 // (6) Teste
-bool end_search(
+void evaluate_population(
     const std::vector<std::vector<double>> &weights,
-    std::vector<std::vector<int>> &population)
+    const std::vector<Individual> population,
+    Individual &best_solution,
+    int &stagnant_count)
 {
-    return false;
+    int best_index = 0;
+
+    for (int i = 1; i < population.size(); i++)
+    {
+        if (population[i].cost < population[best_index].cost)
+        {
+            best_index = i;
+        }
+    }
+
+    if (population[best_index].cost < best_solution.cost)
+    {
+        best_solution = population[best_index];
+        stagnant_count = 0;
+    }
+    else
+    {
+        stagnant_count++;
+    }
 };
 
 template <typename Node>
 TSPResult memetic_search(const IGraph<Node> &graph,
-                               const std::vector<std::vector<double>> &weights,
-                               Node start_node)
+                         const std::vector<std::vector<double>> &weights)
 {
 
-    // Gera população inicial usando heurísticas
-    std::vector<std::vector<int>> population = generate_initial_population(graph, weights);
+    // (1) Inicio
+    std::vector<Individual> population = generate_initial_population(graph, weights);
 
-    improve_population(weights, population, LocalSearchMethod::INVERT, ImprovementType::BEST_IMPROVEMENT);
-}
+    // (2) Fitness
+    calculate_initial_fitness(population, weights);
+
+    // Inicializa variaveis de controle da execução
+    int stagnant_count = 0;
+    Individual best_solution = population[0];
+    std::pair<int, int> last_parents = {-1, -1};
+
+    for (int i = 0; i < MAX_ITERATIONS_NUMBER && stagnant_count < MAX_STAGNANT_ITERATIONS_NUMBER; i++)
+    {
+        // (3) Nova Geração
+        std::vector<Individual> offspring = generate_new_individuas(population, weights, i, last_parents);
+
+        // (4) Busca local
+        improve_individuas(weights, offspring, LocalSearchMethod::INVERT, ImprovementType::BEST_IMPROVEMENT);
+
+        // (5) Renovar
+        renew_population(population, offspring, weights);
+
+        // (6) Teste
+        evaluate_population(weights, population, best_solution, stagnant_count);
+    }
+
+    TSPResult result;
+    result.cost = best_solution.cost;
+    result.path = best_solution.path;
+    return result;
+};
 
 #endif // MEMETIC_SEARCH_H
--- tests/memetic.cpp
+++ tests/memetic.cpp
@@ -13,7 +13,7 @@
 
 int main()
 {
-
+    std::cout << "Hellow" << "\n";
     std::vector<std::string> files = {
         "data/problem_1.csv",
         "data/problem_2.csv",
@@ -36,17 +36,6 @@ int main()
         return 1;
     }
 
-    int start_node = 1;
-
-    std::vector<LocalSearchMethod> methods = {
-        LocalSearchMethod::SWAP,
-        LocalSearchMethod::SHIFT,
-        LocalSearchMethod::INVERT};
-
-    std::vector<ImprovementType> improvements = {
-        ImprovementType::FIRST_IMPROVEMENT,
-        ImprovementType::BEST_IMPROVEMENT};
-
     for (const auto &filename : files)
     {
         DirectedAdjacencyListGraph<int> graph;
@@ -64,36 +53,15 @@ int main()
 
         output << "\nResults for file: " << filename << "\n";
 
-        auto population = generate_population(graph, weights);
-        calculate_initial_fitness(population, weights);
+        auto memeticResult = memetic_search(graph, weights);
 
-        output << "[Initial Population]\n";
-        for (size_t i = 0; i < population.size(); i++)
-        {
-            double cost = calculate_path_cost(weights, population[i].path);
-            output << "Individual " << i + 1 << ": Cost = " << cost << ", Path = ";
-            for (const auto &node : population[i].path)
-            {
-                output << graph.get_node(node) << " ";
-            }
-            output << "\n";
+        output << "[Memetic Algorithm]\n";
+        output << "Cost: " << memeticResult.cost << "\n";
+        output << "Path: ";
+        for (const auto& node : memeticResult.path) {
+            output << graph.get_node(node) << " ";
         }
-
-        improve_population(weights, population, LocalSearchMethod::INVERT, ImprovementType::BEST_IMPROVEMENT);
-
-        output << "[Improved Population after Local Search]\n";
-        for (size_t i = 0; i < population.size(); i++)
-        {
-            double cost = calculate_path_cost(weights, population[i].path);
-            output << "Individual " << i + 1 << ": Cost = " << cost << ", Path = ";
-            for (const auto &node : population[i].path)
-            {
-                output << graph.get_node(node) << " ";
-            }
-            output << "\n";
-        }
-
-        print_population(graph, weights, population);
+        output << "\n";
     }
 
     output.close();

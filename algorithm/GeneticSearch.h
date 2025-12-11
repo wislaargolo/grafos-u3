b9430b90dd53f8e30685a8261a00aaff0bddc48d
Ádisson Téjo <adissonmprtejo@gmail.com>
2025-12-11 20:49:09 -0300

    add comments

--- algorithm/GeneticSearch.h
+++ algorithm/GeneticSearch.h
@@ -11,41 +11,61 @@
 #include "NearestNeighbor.h"
 #include "../utils/TSPUtils.h"
 
+// Tamanho da população durante o algoritmo genético
 #define POPULATION_SIZE 500
+// Número de iterações realizadas pelo algoritmo
 #define MAX_ITERATIONS_NUMBER 10000
-#define MAX_STAGNANT_ITERATIONS_NUMBER 10000
+// Percentual de mutação dos indivíduos
 #define MUTATION_PERCENT 0.5
 
+/**
+ * @brief Estrutura que armazena um indivíduo da população, com a solução "path referente a ele", seu custo e fitness
+ */
 struct Individual {
   std::vector<int> path;
   double cost;
   double fitness;
 };
 
+/**
+ * @brief Gera uma solução aleatória
+ * @param order Ordem do grafo a ser considerado, ou seja, número de nós da solução gerada
+ * @return Solução gerada aleatoriamente representada por vetor de tamanho igual a order
+ */
 std::vector<int> generate_random_path(size_t order) {
     std::vector<int> path(order);
 
+    // Inicializa a lista com elementos de 0 até order
     for (size_t i = 0; i < order; i++) {
         path[i] = i;
     }
 
+    // Embaralha a lista
     std::shuffle(path.begin(), path.end(), std::mt19937{std::random_device{}()});
 
     return path;
 }
 
+/**
+ * @brief Gera a população inicial do algoritmo genético
+ * @param graph Grafo que o algoritmo está utilizando
+ * @param weights Matriz de pesos do grafo utilizado
+ * @return Vetor de indivíduos representando a população inicial
+ */
 template<typename Node>
 std::vector<Individual> generate_population(const IGraph<Node>& graph,
     const std::vector<std::vector<double>>& weights) {
 
     std::vector<Individual> population;
 
+    // Primeiramente insere os resultados dos algoritmos vizinho mais próximo e inserção mais barata na população
     std::vector<int> cheapest_insertion_result = cheapest_insertion(graph, weights, graph.get_node(0));
     std::vector<int> nearest_neighbor_result = nearest_neighbor(graph, weights, graph.get_node(0));
 
     population.push_back({cheapest_insertion_result, -1, -1});
     population.push_back({nearest_neighbor_result, -1, -1});
 
+    // Preenche o restante da população com soluções aleatórias
     for (size_t i = 2; i < POPULATION_SIZE; i++) {
         std::vector<int> random_path = generate_random_path(graph.get_order());
         population.push_back({random_path, -1, -1});
@@ -54,44 +74,79 @@ std::vector<Individual> generate_population(const IGraph<Node>& graph,
     return population;
 }
 
+/**
+ * @brief Calcula o custo e o fitness de cada indivíduo da população
+ * @param population População utilizada pelo algoritmo a ser atualizada
+ * @param weights Matriz de pesos do grafo utilizado
+ */
 void calculate_fitness(std::vector<Individual>& population,
     const std::vector<std::vector<double>>& weights) {
 
+    // Para cada indivíduo, calcula seu custo e depois seu fitness como sendo o inverso do custo
     for (auto& item : population) {
         item.cost = calculate_path_cost(weights, item.path);
         item.fitness = 1 / item.cost;
     }
 }
 
+/**
+ * @brief Seleciona os dois indivíduos com melhor fitness para serem pais
+ * @param population População para se pegar os dois indivíduos
+ * @param last_parents Par com índices dos dois últimos pais selecionados por essa função, para não repetí-los 2 vezes seguidas
+ * @return Par com índices dos dois indivíduos de melhor fitness que são diferentes dos dois em last_parents
+ */
 std::pair<int, int> select_best_parents(const std::vector<Individual>& population, std::pair<int, int> last_parents) {
     int first_better = -1, second_better = -1;
 
+    // Para cada indivíduo que não foi um dos dois últimos pais selecionados
     for (size_t i = 0; i < population.size(); i++) {
         if (i == last_parents.first || i == last_parents.second) {
             continue;
         }
 
+        // Verifica primeiramente se é melhor que o primeiro melhor encontrado até então
         if (first_better == -1 || population[i].fitness > population[first_better].fitness) {
+            // Se sim, atualiza o primeiro e o segundo melhores
             second_better = first_better;
             first_better = i;
-        } else if (second_better == -1 || population[i].fitness > population[second_better].fitness) {
+        }
+        // Se não, verifica se é melhor que o segundo melhor encontrado até então
+        else if (second_better == -1 || population[i].fitness > population[second_better].fitness) {
+            // Se sim, atualiza o segundo melhor
             second_better = i;
         }
     }
 
+    // Retorna um par com os dois melhores fitness diferentes de last_parents
     return std::make_pair(first_better, second_better);;
 }
 
+/**
+ * @brief Seleciona dois indivíduos aleatórios e distintos da população
+ * @param population População para se pegar os dois indivíduos
+ * @return Par com os índices de dois indivíduos aleatórios e distintos
+ */
 std::pair<int, int> select_random_parents(const std::vector<Individual>& population) {
+    // Seleciona aleatoriamente o índice do primeiro indivíduo
     int first_parent = std::rand() % population.size();
+    // Seleciona aleatoriamente o índice segundo indivíduo de forma que não seja igual ao primeiro
     int second_parent = (first_parent + std::rand() % (population.size() - 1) + 1) % population.size();
 
+    // Retorna um par com os índices dos dois indivíduos aleatórios
     return std::make_pair(first_parent, second_parent);
 }
 
+/**
+ * @brief Seleciona os pais que irão reproduzir na iteração atual de maneira híbrida
+ * @param population População para se pegar os dois indivíduos
+ * @param iteration_count Contagem de iterações do algoritmo
+ * @param last_parents Par com os índices dos últimos dois pais selecionados por elitismo
+ * @return Par com índices dos pais escolhidos, através do elitismo ou da aleatoriedade dependendo da iteração
+ */
 std::pair<int, int> select_parents(const std::vector<Individual>& population, int iteration_count,
     std::pair<int, int>& last_parents) {
 
+    // A cada 4 iterações, em 3 a seleção é feita pelo elitismo e em uma a seleção é aleatória
     if (iteration_count % 4 == 3) {
         return select_random_parents(population);
     } else {
@@ -150,8 +205,6 @@ std::vector<int> ordered_crossover(const Individual& first_parent, const Individ
 
     return path;
 }
-
-// ALGORTIMOS DE MUTAÇÃO
 
 /**
  * @brief Realiza a mutação por troca entre dois genes
@@ -277,21 +330,30 @@ std::vector<Individual> renovation_elitism(
     return new_population;
 }
 
+/**
+ * @brief Função que executa o algoritmo genético
+ * @param graph Grafo para ser executado o algoritmo
+ * @param weights Matriz de peso do grafo
+ * @return Melhor solução encontrada durante toda a execução do algoritmo
+ */
 template<typename Node>
 std::vector<int> genetic_search(const IGraph<Node>& graph,
     const std::vector<std::vector<double>>& weights) {
 
     std::srand(std::time(nullptr));
 
+    // Gera e calcula o fitness da população inicial
     std::vector<Individual> population = generate_population(graph, weights);
     calculate_fitness(population, weights);
 
-    int stagnant_count = 0;
+    // Declara a melhor solução atual como sendo a primeira
     Individual best_solution = population[0];
 
+    // Variável utilizada para armazenar os últimos pais a serem selecionados pelo elitismo
     std::pair<int, int> last_parents = {-1, -1};
 
-    for (int i = 0; i < MAX_ITERATIONS_NUMBER && stagnant_count < MAX_STAGNANT_ITERATIONS_NUMBER; i++) {
+    for (int i = 0; i < MAX_ITERATIONS_NUMBER; i++) {
+        // Seleção por elitismo ou aleatoriedade
         std::pair<int, int> parents = select_parents(population, i, last_parents);
 
         // Cruzamento por crossover
@@ -305,6 +367,7 @@ std::vector<int> genetic_search(const IGraph<Node>& graph,
         apply_mutation(child1, MUTATION_PERCENT);
         apply_mutation(child2, MUTATION_PERCENT);
 
+        // Cálculo do fitness dos filhos
         child1.cost = calculate_path_cost(weights, child1.path);
         child2.cost = calculate_path_cost(weights, child2.path);
         child1.fitness = 1 / child1.cost;
@@ -317,17 +380,16 @@ std::vector<int> genetic_search(const IGraph<Node>& graph,
 
         int best_index = 0;
 
+        // Verificação de qual a melhor solução da população
         for (int i = 1; i < population.size(); i++) {
             if (population[i].cost < population[best_index].cost) {
                 best_index = i;
             }
         }
 
+        // Verificação se foi encontrada uma melhor solução do que a melhor até então
         if (population[best_index].cost < best_solution.cost) {
             best_solution = population[best_index];
-            stagnant_count = 0;
-        } else {
-            stagnant_count++;
         }
     }
 
